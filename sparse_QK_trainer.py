46711457dc66ab4fe9833afcd0bbd313b756f410
Keith Wynroe <keithwynroe@Keiths-Air.localdomain>
2024-01-25 04:35:00 +0000

    improved loss fn

--- sparse_QK_trainer.py
+++ sparse_QK_trainer.py
@@ -5,6 +5,7 @@ import transformer_lens
 from transformer_lens import HookedTransformer
 from dataclasses import dataclass
 import math
+from tqdm import tqdm
 import torch.nn.functional as F
 import wandb
 import einops
@@ -46,12 +47,12 @@ class SparseQK(nn.Module):
         acts = F.relu(acts)
         acts = pairwise_product = acts[:, :, :, 0, :] * acts[:, :, :, 1, :]
         reg_loss = self.reg_coeff * t.sqrt(acts.float().abs() + self.eps).sum()
-        feature_fires = (acts > 0).sum()
+        feature_fires = (acts > 0).int()
         score_reconstr = einops.einsum(acts, self.W_dec, "batch posn_q posn_k d_hidden, d_hidden n_heads -> batch posn_q posn_k n_heads")/(self.d_head ** 0.5) + self.b_dec
         score_reconstr = einops.rearrange(score_reconstr, "batch posn_q posn_k n_heads -> batch n_heads posn_q posn_k")
         if masked:
           score_recostr = self.apply_causal_mask(score_reconstr)
-        return score_reconstr, reg_loss, feature_fires, never_fired
+        return score_reconstr, reg_loss, feature_fires
 
     def apply_causal_mask(self, attn_scores: t.Tensor):
         mask = t.triu(t.ones(attn_scores.size(-2), attn_scores.size(-1), device=attn_scores.device), diagonal=1).bool()
@@ -80,14 +81,14 @@ def train_sparse_QK(
     print(f"Training model with {sparse_model.d_hidden} feature pairs.")
     optimizer = t.optim.AdamW(sparse_model.parameters(), lr = 1e-3)
     wandb.init(project="sparse_QK_gpt2_L0.5", entity="kwyn390")
-    feature_tots = t.zeros(cfg.d_hidden)
+    feature_tots = t.zeros(cfg["d_hidden"]).cuda()
     tot_data = 0
     for epoch in range(n_epochs):
         batch_index = None
         progress_bar = tqdm(list(enumerate(data)))
         for batch_idx, batch in progress_bar:
-            if batch_idx < 30000:
-              tot_data += (batch.size(0) * batch.size(1) * batch.size(1))
+            if batch_idx < 10000:
+              tot_data += (batch["tokens"].size(0) * batch["tokens"].size(1) * batch["tokens"].size(1))
               #normalise encoder weights
               sparse_model.renorm_weights()
               optimizer.zero_grad()
@@ -97,29 +98,35 @@ def train_sparse_QK(
               resid_pre = resid_pre/ln
               q, k = cache["q", 10], cache["k", 10]
               original_scores = einops.einsum(q, k, "batch pos_q n_heads d_head, batch pos_k n_heads d_head -> batch n_heads pos_q pos_k").clone()/8
-              modified_output, reg_loss, feature_fires, dead = sparse_model(resid_pre)
-              mse_loss = t.nn.MSELoss(reduction="mean")
-              reconstruction_loss = mse_loss(modified_output, original_scores)
+              modified_output, reg_loss, feature_fires = sparse_model(resid_pre)
+              mse_loss = t.nn.MSELoss(reduction="none")
+              abs_true = t.abs(original_scores)
+              abs_pred = t.abs(modified_output)
+              abs = t.maximum(abs_true, abs_pred)
+              reconstruction_loss = einops.einsum(abs_true, mse_loss(modified_output, original_scores), "batch posn_q n_heads d_head, batch posn_q n_heads d_head ->")
+              reconstruction_loss = reconstruction_loss / (original_scores.size(0)*original_scores.size(1)*original_scores.size(2)**2)
               loss = reconstruction_loss + reg_loss
               loss.backward(retain_graph = True)
               optimizer.step()
               feature_tots += feature_fires.sum(0).sum(0).sum(0)
+              l0 = feature_fires.sum() / (feature_fires.size(0)*(feature_fires.size(1)**2))
               feature_freqs = feature_tots / tot_data
               wandb.log({
                   "recons_score": reconstruction_loss,
                   "loss": loss,
                   "reg_loss": reg_loss,
-                  "l0": feature_fires / (feature_fires.shape(0)*(feature_fires.shape(1)**2)),
+                  "l0": l0,
                   "dead_features": (feature_freqs < cfg["dead_freq"]).sum()
 
               })
+              del batch_idx
+
 
         print(
                 f"Epoch {epoch} reconstruction loss: {reconstruction_loss.item()} l0: {l0} reg_loss {reg_loss}"
             )
         print(f"Epoch {epoch} loss: {loss.item()}")
 
-        del batch_index
-
+      
 
     return sparse_model
